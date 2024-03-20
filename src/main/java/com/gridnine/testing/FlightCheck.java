d628d3f604c20c5040da8beeec312a05ad2bea24
Alexsand74 <1974alexsand@gmail.com>
2024-03-20 16:32:59 +0300

    Third commit

--- src/main/java/com/gridnine/testing/FlightCheck.java
+++ src/main/java/com/gridnine/testing/FlightCheck.java
@@ -1,30 +1,32 @@
 package com.gridnine.testing;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class FlightCheck {
-    public static List<Flight> flightValidation(List<Flight> arrayFlight){
-        if(arrayFlight == null){
+    public static List<Flight> flightValidation(List<Flight> arrayFlight) {
+        if (arrayFlight == null) {
             throw new IllegalArgumentException(
                     //массив имеет значение null
                     "the array has a value null");
         }
 
-         List resultFlight = arrayFlight.stream()
+        List resultFlight = arrayFlight.stream()
                 .filter(flight -> segmentListValidation(flight.getSegments()) == true)
                 .collect(Collectors.toList());
 /**
-*        for (Flight f: arrayFlight) {
-*        if (segmentListValidation( f.getSegments())){
-*                resultFlight.add(f);
-*            }
-*        }
-*/
+ *        for (Flight f: arrayFlight) {
+ *        if (segmentListValidation( f.getSegments())){
+ *                resultFlight.add(f);
+ *            }
+ *        }
+ */
         return resultFlight;
     }
-     private static boolean segmentListValidation (List <Segment> flight){
+
+    private static boolean segmentListValidation(List<Segment> flight) {
 //         int counter = 0;
 //         for (Segment s: flight) {
 //            if(!segmentOneValidation(s)){ counter++;}
@@ -34,20 +36,37 @@ public class FlightCheck {
 //          }
 //       return true;
 
-         return flight.stream()
-                      .anyMatch((f) -> segmentOneValidation(f));
-     }
+        if (flight.size() > 1) {
+            long sumOfHours = 0;
+            for (int i = flight.size() - 1; i > 0; i--) {
+//              Дата вылета ---> LocalDateTime departureDate;
+//              Дата прибытия --->  LocalDateTime arrivalDate;
+                LocalDateTime dataMax = flight.get(i).getArrivalDate();
+                Long endSessionSeconds = dataMax.toEpochSecond(ZoneOffset.UTC);
+                LocalDateTime dataMin = flight.get(i - 1).getDepartureDate();
+                Long startSessionSeconds = dataMin.toEpochSecond(ZoneOffset.UTC);
+                sumOfHours += (endSessionSeconds - startSessionSeconds);
+            }
+            System.out.println(" 7200 = " + sumOfHours);
+            return 2 * 60 * 60 < sumOfHours;
+        }
 
-    private static boolean validationTheSegmentForCorrespondenceBetweenDepartureAndArrival (Segment seg){
+        return flight.stream()
+                .anyMatch((f) -> segmentOneValidation(f));
+    }
+
+    private static boolean validationTheSegmentForCorrespondenceBetweenDepartureAndArrival(Segment seg) {
         LocalDateTime thisDay = LocalDateTime.now();
         return (seg.getArrivalDate().isAfter(seg.getDepartureDate()));
     }
-    private static boolean validationTheSegmentForExpiredTime (Segment seg){
+
+    private static boolean validationTheSegmentForExpiredTime(Segment seg) {
         LocalDateTime thisDay = LocalDateTime.now();
         return (seg.getArrivalDate().isAfter(thisDay)
                 && seg.getDepartureDate().isAfter(thisDay));
     }
-     private static boolean segmentOneValidation (Segment seg){
+
+    private static boolean segmentOneValidation(Segment seg) {
         LocalDateTime thisDay = LocalDateTime.now();
         return (seg.getArrivalDate().isAfter(thisDay)
                 && seg.getDepartureDate().isAfter(thisDay)
